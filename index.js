876951bef43b0a886dbe10e5fb6a7b77f2c434d7
Edward Bera <47110064+edwardbera@users.noreply.github.com>
2024-04-21 23:23:12 +0200

    Basic Controls

--- index.js
+++ index.js
@@ -5,40 +5,75 @@ let velocity = 0;
 
 
 let snake = {
-    x : 20,
-    y : 10
+    x : 10,
+    y : GAME_BOARD.height - 20,
+    width : 10,
+    height : 10,
+    direction : 'right'
 }
 
+let food = {
+    x : 10,
+    y : 10,
+    height : 5,
+    width : 5,
+}
+
+
 function drawSnake(){
-    if (hasCollided(snake.x , snake.y)){
-        snake.x = 10;
+    if (hasCollided(snake.x + snake.width , snake.y + snake.height)){
         endGame()
     }
-    CONTEXT.fillRect(snake.x, 10, 50, 10);   
+    CONTEXT.fillRect(snake.x, snake.y, snake.width, snake.height);   
+}
+
+function generateFood(){
+
+    CONTEXT.fillRect(food.x, food.y, food.width, food.height);   
 }
 
 function update(){
     CONTEXT.clearRect(0,0, GAME_BOARD.width , GAME_BOARD.height )
     drawSnake();
-    snake.x += velocity;
+    generateFood();
+    if (snake.direction == 'down'){
+        snake.y += velocity;
+    }else if(snake.direction == 'up'){
+        snake.y -= velocity;
+    }else if(snake.direction == 'right'){
+        snake.x += velocity;
+    }else if(snake.direction == 'left'){
+        snake.x -= velocity;
+    }else{
+        snake.x += velocity;
+    }
+   
 }
 
 let hasCollided = (x, y) =>{
     if (x >= GAME_BOARD.width || x <= 0){
-        return true
+        return true;
     }else if (y >= GAME_BOARD.height || y <= 0 ){
-        return true
+        return true;
+    }
+
+    if (x == food.x  && y == food.y ){
+        return true;
     }
 }
 
 function endGame(){
     CONTEXT.clearRect(0,0, GAME_BOARD.width , GAME_BOARD.height )
     velocity = 0;
+    snake.x = 10;
+    snake.y = GAME_BOARD.height - 20;
     START_BUTTON.disabled = false;
 }
 
 function startGame(){
-    CONTEXT.clearRect(0,0, GAME_BOARD.width , GAME_BOARD.height )
+    food.x = Math.random() * 400;
+    food.y = Math.random() * 300;
+    CONTEXT.clearRect(0,0, GAME_BOARD.width , GAME_BOARD.height);
     velocity = 0.5;
 }
 
@@ -47,9 +82,32 @@ START_BUTTON.addEventListener("click", function(){
     this.disabled = true;
 })
 
-function gameloop(){
-    update();
-    window.requestAnimationFrame(gameloop);
+function controls(){
+    document.addEventListener('keydown', function(event){
+        switch (event.key){
+            case "ArrowDown":
+                snake.direction = 'down';
+                break;
+            case "ArrowUp":
+                snake.direction = 'up';
+                break;
+            case "ArrowLeft":
+                snake.direction = 'left';
+                break;
+            case "ArrowRight":
+                snake.direction = 'right';
+                break;
+            default:
+                return;
+        }
+    });
 }
 
-gameloop()
+function gameLoop(){
+    
+    controls()
+    update();
+    window.requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
