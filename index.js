98e6f24e6f19b2b68fde1394a21332c695bae4e5
Edward Bera <47110064+edwardbera@users.noreply.github.com>
2024-04-23 15:22:04 +0200

    Tail movement

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const START_BUTTON = document.getElementById("startButton");
 const SCORE_OUTPUT = document.getElementById("score");
 let velocity = 0;
 let score = 0;
-
+let turningPoint = 0;
 SCORE_OUTPUT.innerText = score;
 
 
@@ -55,40 +55,76 @@ function generateFood(){
 function update(){
     CONTEXT.clearRect(0,0, GAME_BOARD.width , GAME_BOARD.height )
     drawSnake();
-    //drawTail();
+    drawTail();
     if (hasCollidedWithFood(snake.x  , snake.y )){
         food.x = Math.random() * 400;
         food.y = Math.random() * 300;
     } 
         generateFood();
+        snakeMovement();
+        tailMovement();
+    
+}
+
+let snakeMovement = () =>{
+        
     if (snake.direction == 'down'){
-        snake.y += velocity;
-        tails.forEach((tail)=>{
-            tail.y += velocity;
-        })
+        snake.y += velocity; 
         //snakeTail.y += velocity;
     }else if(snake.direction == 'up'){
-        snake.y -= velocity;
-        tails.forEach((tail)=>{
-            tail.y -= velocity;
-        })
-        //snakeTail.y -= velocity;
+        
+        snake.y -= velocity; 
     }else if(snake.direction == 'right'){
         snake.x += velocity;
-        tails.forEach((tail)=>{
-            tail.x += velocity;
-        })
+        
         //snakeTail.x += velocity;
     }else if(snake.direction == 'left'){
         snake.x -= velocity;
-        tails.forEach((tail)=>{
-            tail.x -= velocity;
-        })
+        
         //snakeTail.x += velocity;
     }else{
         snake.x += velocity;
     }
    
+}
+function tailMovement(){
+  
+    tails.forEach((tail)=>{
+        if ( tail.direction == "right" && turningPoint != tail.x || tail.direction == "right" && turningPoint == 0){
+            tail.x += velocity;
+        }else if (tail.direction =="left" && turningPoint != tail.x || tail.direction =="left" && turningPoint == 0){
+            tail.x -= velocity;
+          
+        }else if ( tail.direction == "right" || tail.direction == "left"  && turningPoint == tail.x ){
+            if (snake.direction == "up"){
+                tail.direction = "up"
+                tail.y -= velocity;
+                turningPoint = 0
+            }else if (snake.direction == "down"){
+                tail.direction = "down"
+                tail.y += velocity;
+                turningPoint = 0
+            }  
+        }else if ( tail.direction == "down" && turningPoint != tail.y || tail.direction == "down" && turningPoint == 0 ){
+            tail.y += velocity;
+            
+        }else if ( tail.direction == "up" && turningPoint != tail.y || tail.direction == "up" && turningPoint == 0 ){
+            tail.y -= velocity;
+            
+        }else if ( tail.direction == "down" || tail.direction == "up"  && turningPoint == tail.y ){
+
+            if (snake.direction == "right"){
+                tail.direction = "right"
+                tail.x += velocity;
+                turningPoint = 0
+            }else if (snake.direction == "left"){
+                tail.direction = "left"
+                tail.x -= velocity;
+                turningPoint = 0
+            }  
+            }
+
+    })
 }
 
 let hasCollidedWithWalls = (x, y) =>{
@@ -105,9 +141,39 @@ let hasCollidedWithFood = (x, y) =>{
          (y + 5) > food.y  && (y -5 )< food.y + food.height
      ){
         let tail = Object.create(snakeTail);
-        tail.x = snake.x + 10;
-        tail.direction = snake.direction;
-        tail.y = snake.y
+        if (tails.length == 0 && snake.direction == "right"){
+            tail.x = snake.x - (snake.width * (tails.length + 1));
+            tail.direction = snake.direction;
+            tail.y = snake.y
+        }else if (tails.length == 0 && snake.direction == "left"){ 
+            tail.x = snake.x + (snake.width * (tails.length + 1));
+            tail.direction = snake.direction;
+            tail.y = snake.y
+        }else if(tails.length > 0 && snake.direction == "right"){
+            tail.x = snake.x - (snake.width * (tails.length + 1));
+            tail.direction = snake.direction;
+            tail.y = tails[tails.length-1].y
+        }else if(tails.length > 0 && snake.direction == "left"){
+            tail.x = snake.x + (snake.width * (tails.length + 1));
+            tail.direction = snake.direction;
+            tail.y = tails[tails.length-1].y
+        }else if (tails.length == 0 && snake.direction == "up"){
+            tail.y = snake.y + (snake.height * (tails.length + 1));
+            tail.direction = snake.direction;
+            tail.x = snake.x
+        }else if (tails.length == 0 && snake.direction == "down"){ 
+            tail.y = snake.y - (snake.height * (tails.length + 1));
+            tail.direction = snake.direction;
+            tail.x = snake.x
+        }else if(tails.length > 0 && snake.direction == "up"){
+            tail.y = snake.y + (snake.height * (tails.length + 1));
+            tail.direction = snake.direction;
+            tail.x = tails[tails.length-1].x
+        }else if(tails.length > 0 && snake.direction == "down"){
+            tail.y = snake.y - (snake.height * (tails.length + 1));
+            tail.direction = snake.direction;
+            tail.x = tails[tails.length-1].x
+        }
         tails.push(tail)
         updateScore();
         //console.log(tails.length)
@@ -130,6 +196,7 @@ function startGame(){
     food.y = Math.random() * 300;
     CONTEXT.clearRect(0,0, GAME_BOARD.width , GAME_BOARD.height);
     velocity = 1;
+    turningPoint = 0;
 }
 
 function updateScore(){
@@ -147,15 +214,22 @@ function controls(){
         switch (event.key){
             case "ArrowDown":
                 snake.direction = 'down';
+                turningPoint = snake.x;
                 break;
             case "ArrowUp":
                 snake.direction = 'up';
+              
+                turningPoint = snake.x;
                 break;
             case "ArrowLeft":
                 snake.direction = 'left';
+              
+                turningPoint= snake.y;
                 break;
             case "ArrowRight":
                 snake.direction = 'right';
+               
+                turningPoint = snake.y;
                 break;
             default:
                 return;
